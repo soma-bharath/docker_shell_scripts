24fcb52fbf81014a87032b9f315b62b253c54f82
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2023-09-28 11:26:35 +0530

    Update conatinerstatus.sh

--- conatinerstatus.sh
+++ conatinerstatus.sh
@@ -11,6 +11,3 @@ else
    sudo docker start $con
    echo "check now"
 fi
-https://tk.notifications.epam.com/track?eId=4c89010f-8c87-448d-a284-2a81110e7d70&nId=18888435&ch=Email&uri=https%3a%2f%2finterview-public.epam.com%2fpublic%3fticket%3dbd46fdcc-fbee-4d05-8e70-beaca517a008
-
-https://tk.notifications.epam.com/track?eId=4c89010f-8c87-448d-a284-2a81110e7d70&nId=18888435&ch=Email&uri=https%3a%2f%2finterview-public.epam.com%2fpublic%3fticket%3dbd46fdcc-fbee-4d05-8e70-beaca517a008
